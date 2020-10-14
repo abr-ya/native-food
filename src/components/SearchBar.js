4d00ceb46f0a9f8628b31a553b7fc451ca6e600e
Abr-ya <gamer05@list.ru>
2020-10-14 22:57:03 +0300

    Update SearchBar styles

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,35 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { TextInput, StyleSheet, View } from "react-native";
+import { FontAwesome } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View>
-      <Text>Search Bar Component</Text>
+    <View style={styles.backgroundStyle}>
+      <FontAwesome name="search" style={styles.iconStyle} />
+      <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: '#DDD',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 20,
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 20,
+  },
+  iconStyle: {
+    color: 'grey',
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 10,
+  }
 });
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import SearchBar from '../components/SearchBar';
 import PlaceList from '../components/PlaceList';
 
 const SearchScreen = () => {
   return (
     <View>
-      <Text style={styles.textStyle1}>Search Screen</Text>
       <SearchBar />
       <PlaceList />
     </View>
